ce1ea4211926874fc504db9a454f161ec9f109d9
Pathi_rao <lakshmipathi0000@gmail.com>
2021-09-28 13:28:24 +0200

    Update datahandler.py

--- Siamese_NNs/AT&T_anomaly_detection/datahandler.py
+++ Siamese_NNs/AT&T_anomaly_detection/datahandler.py
@@ -8,9 +8,6 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, transforms
 
 
-training_dir = '..\Dataset\AT&T_Face\Train'
-testing_dir = '..\Dataset\AT&T_Face\Test'
-
 class SiameseNetworkDataset(Dataset):
     
     def __init__(self, imageFolderDataset, transform=None, should_invert=True):
@@ -25,15 +22,15 @@ class SiameseNetworkDataset(Dataset):
         should_get_same_class = random.randint(0,1) 
 
         if should_get_same_class: # Enter the loop if value is 1
-            # while True:
-            for i in range(0, 1):
+            while True:
+            # for i in range(0, 1):
                 #keep looping till the same class image is found
                 img1_tuple = random.choice(self.imageFolderDataset.imgs) 
                 if img0_tuple[1] == img1_tuple[1]:
                     break
         else:
-            # while True:
-            for i in range(0, 1):
+            while True:
+            # for i in range(0, 1):
                 #keep looping till a different class image is found
                 img1_tuple = random.choice(self.imageFolderDataset.imgs) 
                 if img0_tuple[1] !=img1_tuple[1]:
@@ -60,25 +57,39 @@ class SiameseNetworkDataset(Dataset):
             img0 = self.transform(img0) # apply the given tranformation(when instantiating the class).
             img1 = self.transform(img1)
 
-        return img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])], dtype=np.float32))
+        return img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])], dtype=np.float32)) # images and label
     
     def __len__(self):
         return len(self.imageFolderDataset.imgs)
 
 
 
-def pre_preocessor():
+def pre_preocessor(root_dir, batchsize):
 
-    folder_dataset = datasets.ImageFolder(root=training_dir)
+    train_data = datasets.ImageFolder(root_dir+'Train')
+    test_data = datasets.ImageFolder(root_dir+'Test')
     # print(folder_dataset.imgs) 
     # o/p will be tuple with each image path and the folder index
     # [('..\\..\\..\\Dataset\\AT&T_Face\\Train\\s1\\1.pgm', 0), ('..\\..\\..\\Dataset\\AT&T_Face\\Train\\s1\\10.pgm', 0)....]  
 
-    siamese_dataset = SiameseNetworkDataset(imageFolderDataset = folder_dataset,
+    siamese_train_dataset = SiameseNetworkDataset(imageFolderDataset = train_data,
                                             transform=transforms.Compose([transforms.Resize((100,100)),
                                             # we can also use custom collate function to resize with padding if have different resolution
-                                                                            transforms.ToTensor()
-                                                                            ])
+                                                                            transforms.ToTensor(),
+                                                                            transforms.Normalize([0.5], [0.5])])
                                             ,should_invert=True)
 
-    return siamese_dataset
+    siamese_test_dataset = SiameseNetworkDataset(imageFolderDataset = test_data,
+                                            transform=transforms.Compose([transforms.Resize((100,100)),
+                                            # we can also use custom collate function to resize with padding if have different resolution
+                                                                            transforms.ToTensor(),
+                                                                            transforms.Normalize([0.5], [0.5])])
+                                            ,should_invert=False)
+
+    # create the dataloaders
+    train_loader = torch.utils.data.DataLoader(siamese_train_dataset, batch_size=batchsize,
+                                            shuffle=True)
+    test_loader = torch.utils.data.DataLoader(siamese_test_dataset, batch_size=batchsize,
+                                            shuffle=False)
+
+    return train_loader, test_loader
